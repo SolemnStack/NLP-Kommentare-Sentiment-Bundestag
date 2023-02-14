2f592b03616c9d808c3c9453aec25405f381b1f0
swether <swether@students.uni-mainz.de>
2023-02-14 15:07:04 +0100

    code now correctly splits comments if multiple show up in a single comment tag. also dictionary is now saved to a json file

--- generate_dictionary.py
+++ generate_dictionary.py
@@ -4,6 +4,8 @@ import os
 import xml.etree.ElementTree as ET
 # RegEx
 import re
+# Used to save dictionary to json file
+import json
 
 # Reads all files in BundestagOpenData and save Path into a list (path_to_files)
 path_to_folder = 'BundestagOpenData/'
@@ -68,118 +70,6 @@ for key in rededict:
     print(key, '->', rededict[key])
     print('')
 
-##########################################################
-############ Hier folgen meine Testversuche ##############
-##########################################################
-
-'''
-import requests
-
-plenarliste = []
-wahlperiode = 20
-protokollnummer = 1
-listennummer = 0
-while protokollnummer < 84:
-    response = requests.get("https://search.dip.bundestag.de/api/v1/plenarprotokoll-text?f.dokumentnummer=" + str(wahlperiode) + "/" + str(protokollnummer) + "&apikey=GmEPb1B.bfqJLIhcGAsH9fTJevTglhFpCoZyAAAdhp")
-    if protokollnummer % 5 == 0:
-        response.json()
-    plenarliste.append(response.json()['documents'][0]['text'])
-    #plenarliste[listennummer] = response.json()['documents'][0]['text']
-    protokollnummer += 1
-
-print(plenarliste)
-'''
-
-'''
-print("Root Tag: " + str(root.tag))
-print("Root Attribute: " + str(root.attrib))
-print('')
-print("Children and Attributes of Root:")
-for child in root:
-    print(child.tag, child.attrib)
-
-print('')
-print(root[1][0][0].text)
-
-# Printed die ID jeder Rede im XML-Dokument
-print("IDs aller Reden im Dokument:")
-for rede in root.iter('rede'):
-    print(rede.attrib)
-
-print('')
-
-print("IDs aller Tagesordnungspunkte im Dokument:")
-for tagesordnungspunkt in root.iter('tagesordnungspunkt'):
-    print(tagesordnungspunkt.attrib)
-
-print('')
-
-print("Inhalt aller Tagesordnungspunkte im Dokument:")
-for tagesordnungspunkt in root.iter('tagesordnungspunkt'):
-    print(tagesordnungspunkt.findall('p'))
-    print(type(tagesordnungspunkt.findall('p')))
-    print('')
-
-print('')
-
-testliste = tagesordnungspunkt.findall('p')
-
-for textl in testliste:
-    print(textl.text)
-
-print('')
-
-print("Inhalt aller Reden im Dokument:")
-for rede in root.iter('rede'):
-    print(rede.findall('p'))
-    print(type(rede.findall('p')))
-    print('')
-
-print('')
-print("Redeinhalt")
-print('')
-
-testliste = rede.findall('p')
-
-print(testliste)
-
-for textl in testliste:
-    print(textl.text)
-'''
-
-# Test um Redeinhalte in eine Liste zu speichern
-#redeliste = list()
-#for i, tagesordnung in enumerate(root.iter('tagesordnung')):
-#    redeliste.append(i)
-#    for rede in root.iter('rede'):
-#        redeliste[i].append(rede.findall('p'))
-
-# Speichert alle Beiträge in Reden in eine Redeliste
-# Einzelne Reden danach erreichbar mit redeliste[x]
-
-#print('Die gesamte Redeliste')
-#print(redeliste)
-#print('')
-#print("Ausschnitt aus Redeliste")
-#print(redeliste[0])
-#print('')
-
-# Iteriert durch alle Reden hindurch und printet den Inhalt
-#for rede in redeliste:
-#    #print(rede)
-#    #print(rede.attrib)
-#    #print('')
-#    for text in rede:
-#        print(text.text)
-#        print('')
-
-# TODO:: Dictionary anlegen mit RedeID : Redeinhalt
-
-# Printed die ID jeder Rede im XML-Dokument
-# print("IDs aller Reden im Dokument:")
-# for rede in root.iter('rede'):
-#     print(rede.attrib)
-
-# TODO:: Kommentare extrahieren. Dictionary mit Person : Kommentarinhalt
-
-# TODO:: Dictionary mit RedeID, Fraktion des Redners, Redeinhalt, und falls vorhanden Kommentare/Beifälle und deren Fraktionen
+# Saves dictionary as json file
+out_file = open("BundestagsReden.json", "w", encoding='utf8')
+json.dump(rededict, out_file, indent = 4, ensure_ascii=False)
